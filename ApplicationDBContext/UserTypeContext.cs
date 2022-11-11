42230007fc8c247d2106aad963413bd3a8604417
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-11-11 12:06:35 -0500

    Add project files.

---
+++ ApplicationDBContext/UserTypeContext.cs
@@ -0,0 +1,12 @@
+﻿using Club_in_API.UserType.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Club_in_API.UserType.ApplicationDBContext
+{
+    public class UserTypeContext : DbContext
+    {
+        public UserTypeContext(DbContextOptions<UserTypeContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Club_in_API.UserType.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+        public string UserType { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
